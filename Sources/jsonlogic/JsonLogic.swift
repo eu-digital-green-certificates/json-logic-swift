7ed3c9f6ccbb21d42a3b2555b25332a8a4bf58b5
Nick Guendling <hello@nickguendling.com>
2022-01-10 13:17:51 +0100

    Restructure JsonLogic into multiple files

--- Sources/jsonlogic/JsonLogic.swift
+++ Sources/jsonlogic/JsonLogic.swift
@@ -5,67 +5,11 @@
 import Foundation
 import JSON
 
-///  Errors that can be thrown from JsonLogic methods
-public enum JSONLogicError: Error, Equatable {
-    public static func == (lhs: JSONLogicError, rhs: JSONLogicError) -> Bool {
-        switch lhs {
-        case let canNotParseJSONData(ltype):
-            if case let canNotParseJSONData(rtype) = rhs {
-                return ltype == rtype
-            }
-            return false
-        case let canNotConvertResultToType(ltype):
-            if case let canNotConvertResultToType(rtype) = rhs {
-                return ltype == rtype
-            }
-            return false
-        case let .canNotParseJSONRule(ltype):
-            if case let canNotParseJSONRule(rtype) = rhs {
-                return ltype == rtype
-            }
-            return false
-        }
-    }
-
-    /// Invalid json data was passed
-    case canNotParseJSONData(String)
-
-    /// Invalid json rule was passed
-    case canNotParseJSONRule(String)
-
-    /// Could not convert the result from applying the rule to the expected type
-    case canNotConvertResultToType(Any.Type)
-}
-
-/**
-    A shortcut method to parse and apply a json logic rule.
-
-    If you need to apply the same rule to multiple json data, it is more efficient to
-    instantiate a `JsonLogic` class that will cache and reuse the parsed rule.
-*/
-public func applyRule<T>(_ jsonRule: String, to jsonDataOrNil: String? = nil) throws -> T {
-    return try JsonLogic(jsonRule).applyRule(to: jsonDataOrNil)
-}
-
-public func applyRule<T>(_ jsonRule: JSON, to jsonDataOrNil: String? = nil) throws -> T {
-    return try JsonLogic(jsonRule).applyRule(to: jsonDataOrNil)
-}
-public func applyRule<T>(_ jsonRule: JSON, to jsonOrNil: JSON? = nil) throws -> T {
-    return try JsonLogic(jsonRule).applyRuleInternal(to: jsonOrNil)
-}
-
-public func applyRule<T>(_ jsonRule: JSON, to jsonDataOrNil: String? = nil, customOperators: [String: (JSON?) -> JSON]?) throws -> T {
-    return try JsonLogic(jsonRule, customOperators: customOperators).applyRule(to: jsonDataOrNil)
-}
-
-public func applyRule<T>(_ jsonRule: JSON, to jsonOrNil: JSON? = nil, customOperators: [String: (JSON?) -> JSON]?) throws -> T {
-    return try JsonLogic(jsonRule, customOperators: customOperators).applyRuleInternal(to: jsonOrNil)
-}
-
 /**
     It parses json rule strings and executes the rules on provided data.
 */
 public final class JsonLogic {
+
     // The parsed json string to an Expression that can be used for evaluation upon specific data
     private let parsedRule: Expression
 
@@ -88,6 +32,7 @@ public final class JsonLogic {
     public convenience init(_ jsonRule: String) throws {
         try self.init(jsonRule, customOperators: nil)
     }
+
     public convenience init(_ jsonRule: JSON) throws {
         try self.init(jsonRule, customOperators: nil)
     }
@@ -139,10 +84,10 @@ public final class JsonLogic {
         if let jsonDataOrNil = jsonDataOrNil {
             jsonData = JSON(string: jsonDataOrNil)
         }
-         return try self.applyRuleInternal(to: jsonData)
+         return try self.applyRule(to: jsonData)
     }
     
-    public func applyRuleInternal<T>(to jsonData: JSON? = nil) throws -> T {
+    public func applyRule<T>(to jsonData: JSON? = nil) throws -> T {
         let result = try parsedRule.evalWithData(jsonData)
 
         let convertedToSwiftStandardType = try result.convertToSwiftTypes()
@@ -153,10 +98,11 @@ public final class JsonLogic {
                 print(" canNotConvertResultToType \(T.self) from \(type(of: value))")
                 throw JSONLogicError.canNotConvertResultToType(T.self)
             }
+
             return convertedResult
         default:
             // workaround for swift bug that cause to fail when casting
-            // from generic type that resolves to Ant? to Any? in certain compilers, see SR-14356
+            // from generic type that resolves to Any? in certain compilers, see SR-14356
             #if compiler(>=5) && swift(<5)
             guard let convertedResult = (convertedToSwiftStandardType as Any) as? T else {
                 // print("canNotConvertResultToType \(T.self) from \(type(of: convertedToSwiftStandardType))")
@@ -168,35 +114,9 @@ public final class JsonLogic {
                 throw JSONLogicError.canNotConvertResultToType(T.self)
             }
             #endif
+
             return convertedResult
         }
     }
-}
 
-extension JSON {
-    func convertToSwiftTypes() throws -> Any? {
-        switch self {
-        case .Error:
-            throw JSONLogicError.canNotParseJSONData("\(self)")
-        case .Null:
-            return Optional<Any>.none
-        case .Bool:
-            return self.bool
-        case .Int:
-            return Swift.Int(self.int!)
-        case .Double:
-            return self.double
-        case .String:
-            return self.string
-        case .Date:
-            return self.date
-        case let JSON.Array(array):
-            return try array.map { try $0.convertToSwiftTypes() }
-        case .Dictionary:
-            let o = self.dictionary!
-            return try o.mapValues {
-                try $0.convertToSwiftTypes()
-            }
-        }
-    }
 }
